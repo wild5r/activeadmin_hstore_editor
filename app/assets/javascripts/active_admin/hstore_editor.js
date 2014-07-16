ccbc2ff039f7aa72a5a86783a1099aad3e9fe493
wild <wild@portal>
2014-07-16 12:06:18 +0400

    change indent level

--- app/assets/javascripts/active_admin/hstore_editor.js
+++ app/assets/javascripts/active_admin/hstore_editor.js
@@ -2,21 +2,21 @@
 //= require jsoneditor/asset/jsonlint/jsonlint.js
 
 ;(function(window, $) {
-		$(function() {
-				/*  multipart/form-data*/
-				$('form').attr('enctype', 'application/json');
+  $(function() {
+    /*  multipart/form-data*/
+    $('form').attr('enctype', 'application/json');
 
-				$('div.jsoneditor-wrap').each(function(i,wrap){
-						var container = $(wrap)[0];
-						var textarea = $($(wrap).find('textarea'));
-						var editor;
-						var options = {
-								mode: 'tree',
-								change: function(ev){
-										textarea.text(JSON.stringify(editor.get()));
-								}
-						};
-						editor = new JSONEditor(container, options,JSON.parse(textarea.val()));
-				});
-		})
+    $('div.jsoneditor-wrap').each(function(i,wrap){
+      var container = $(wrap)[0];
+      var textarea = $($(wrap).find('textarea'));
+      var editor;
+      var options = {
+        mode: 'tree',
+        change: function(ev){
+          textarea.text(JSON.stringify(editor.get()));
+        }
+      };
+      editor = new JSONEditor(container, options,JSON.parse(textarea.val()));
+    });
+  })
 })(window, jQuery)
