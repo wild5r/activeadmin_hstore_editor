4b2d8e92f9d250ec4b6d7713f49fb0af321f32fc
wild <wild@portal>
2014-07-29 01:57:13 +0400

    raise exception when request parameters not found

--- lib/activeadmin/hstore_editor/version.rb
+++ lib/activeadmin/hstore_editor/version.rb
@@ -1,5 +1,5 @@
 module ActiveAdmin
   module HstoreEditor
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
--- lib/activeadmin/resource_dsl.rb
+++ lib/activeadmin/resource_dsl.rb
@@ -5,8 +5,13 @@ module ActiveAdmin
   class ResourceDSL
     def hstore_editor
       before_save do |object,args|
-        object.class.columns_hash.select {|key,attr| attr.type == :hstore}.keys.each do |key|
-          object.attributes = {key => JSON.parse(params[object.class.name.underscore][key])} if params[object.class.name.underscore].key? key
+        request_namespace = object.class.name.underscore
+        if params.key? request_namespace
+          object.class.columns_hash.select {|key,attr| attr.type == :hstore}.keys.each do |key|
+            object.attributes = {key => JSON.parse(params[request_namespace][key])} if params[request_namespace].key? key
+          end
+        else
+          raise ActionController::ParameterMissing, request_namespace
         end
       end
     end
