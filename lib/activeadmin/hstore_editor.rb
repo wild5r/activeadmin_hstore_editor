29b1466257a765ff1acee98bad7b0ab805515fb0
wild <wild@portal.local>
2014-08-27 17:49:14 +0400

    make nodigest copy

--- lib/activeadmin/hstore_editor.rb
+++ lib/activeadmin/hstore_editor.rb
@@ -5,6 +5,18 @@ require "activeadmin/resource_dsl"
 module ActiveAdmin
   module HstoreEditor
     class Engine < ::Rails::Engine
+      config.assets.precompile += %w[img/jsoneditor-icons.png]
+
+      rake_tasks do
+        task 'assets:precompile' do
+          fingerprint = /\-[0-9a-f]{32}\./
+          Dir['public/assets/img/jsoneditor-icons-*'].each do |file|
+            next unless file =~ fingerprint
+            nondigest = file.sub fingerprint, '.'
+            FileUtils.cp file, nondigest, verbose: true
+          end
+        end
+      end
     end
   end
 end
